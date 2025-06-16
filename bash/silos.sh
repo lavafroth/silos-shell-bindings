0edb448f0f9a4958ad4e978e2f27051b6090be77
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-06-16 15:50:50 +0530

    feat: use fd 1 for stdout

--- bash/silos.sh
+++ bash/silos.sh
@@ -2,8 +2,7 @@ silos () {
   INSH="\"$@ in sh\""
   (curl http://localhost:8000/api/v1/get --silent --json "{ \"desc\": $INSH }" | jq -r .[0] | base64 -w0 | \
   python -c "import fcntl; import termios; import base64;
-with open('/dev/stdout', 'w') as fd:
-  for c in base64.b64decode(input()).decode().strip(): fcntl.ioctl(fd, termios.TIOCSTI, c)" &)
+for c in base64.b64decode(input()).decode().strip(): fcntl.ioctl(1, termios.TIOCSTI, c)" &)
 }
 
 bind '"\C-g": "\C-asilos \e[C\C-m"'
