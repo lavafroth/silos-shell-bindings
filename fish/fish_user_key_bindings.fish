4dcab1f7f117d8012bc3db768eeb80cc71a84903
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-06-16 16:37:38 +0530

    feat: fish supports multiline editing via killring

--- fish/fish_user_key_bindings.fish
+++ fish/fish_user_key_bindings.fish
@@ -1,3 +1,8 @@
+function silos_interactive
+    commandline -f backward-kill-line
+    commandline --insert (silos $fish_killring[1])
+end
+
 function fish_user_key_bindings
-    bind ctrl-g 'silos (commandline --current-buffer)'
+    bind ctrl-g silos_interactive
 end
--- fish/silos.fish
+++ fish/silos.fish
@@ -1,4 +1,3 @@
 function silos
-    set SILOS_OUTPUT (curl http://localhost:8000/api/v1/get --silent --json "{ \"desc\": \"$argv in fish\" }" | jq -r .[0])
-    commandline --replace "$SILOS_OUTPUT"
+    curl http://localhost:8000/api/v1/get --silent --json "{ \"desc\": \"$argv in fish\" }" | jq -r .[0]
 end
